58844a4999988d7251a67d46c8f25552e0391d0f
djp1-hub <djp1@yandex.ru>
2021-11-11 14:43:31 +0700

    add six tasks

---
+++ lesson_3_part_3.py
@@ -0,0 +1,6 @@
+def my_func(a,b,c):
+    total=sorted([a,b,c])
+    total.pop(0)
+    print(sum(total))
+
+my_func(7,-8,7)
---
+++ lesson_3_part_6.py
@@ -0,0 +1,14 @@
+def int_func(word):
+    first=word[0]
+    return( first.upper() + word[1:] )
+
+
+print(int_func('text'))
+
+user_input=input('input string with spaces: ').split()
+result = ''
+for i in user_input:
+    result += str(int_func(i)) + ' '
+
+print(result)
+
