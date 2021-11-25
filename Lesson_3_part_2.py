c3df5fa9a8fa32380eed162959a47bd3fb40d094
djp1-hub <69582216+djp1-hub@users.noreply.github.com>
2021-11-25 12:49:35 +0700

    Delete Lesson_3_part_2.py

--- Lesson_3_part_2.py
+++
@@ -1,6 +0,0 @@
-def userinfo(name, family_name, city, birth, email, phone_number):
-    print(
-        f"Потльзователь {name} {family_name} живет в городе {city}, родился в {birth} году, номер телефона: {phone_number}, email: {email}.")
-
-
-userinfo(family_name="Сидоров", email="ya@ya.ru", name="Иван", city="Москва", birth=1972, phone_number="+79232900252")
